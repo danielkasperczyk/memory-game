2c06ae3624ecf383fd9ee3b707b13aee1c34df66
danielkasperczyk <danielkasperczyk@gmail.com>
2020-11-30 21:22:35 +0100

    made into starter file

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-const levels = Array.from(document.querySelectorAll('.levels'));
+import { pickLevel } from './handlers.js';
+import { levels } from './queries.js';
+import { showRecords } from './unit.js'
 
-const test = {
-    4: 6,
-    8: 10,
-    12: 14,
-    16:20,
-    20: 27
-}
-localStorage.setItem('test', JSON.stringify(test) );
-
-function showRecords(){
-    const storage = JSON.parse(localStorage.getItem('test'));
-    const isStorageEmpty = Object.values(storage).filter(val => val > 0)
-
-    if(isStorageEmpty.length > 0){
-        console.log(storage);
-    }
-}
+levels.forEach(level => level.addEventListener('click', pickLevel))
 window.addEventListener('load', showRecords);
-//jeżeli w LocalStorage będzie napisane w ile ruchów użytkwnik wygrał, należy to wyświetlić
-//a jeśli nie ma to nie należy wyświetlać
-
-//po kliknięciu na przycisk pobrać ilość kart do wyświetlenia na planszy
