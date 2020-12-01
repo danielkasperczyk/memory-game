8334f61fd8a59dcbd98a106044efe2b4f55d3c43
danielkasperczyk <danielkasperczyk@gmail.com>
2020-12-01 18:31:41 +0100

    added few queriesSelector

--- queries.js
+++ queries.js
@@ -1,1 +1,4 @@
 export const levels = Array.from(document.querySelectorAll('.level'));
+export const menu = document.querySelector('.menu');
+export const game = document.querySelector('.game');
+export const gameBoard = document.querySelector('.game__cards');
