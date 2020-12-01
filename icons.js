fce9e94058cbbb4ed4d581b03b044a10dd123f28
danielkasperczyk <danielkasperczyk@gmail.com>
2020-12-01 18:33:06 +0100

    object with icons moved here

---
+++ icons.js
@@ -0,0 +1,51 @@
+export const icons = [
+    {
+        animal: 'cat',
+        icon:  `<i class="fas fa-cat"></i>`
+    },
+    {
+        animal:'crow', 
+        icon: `<i class="fas fa-crow"></i>`
+    },
+    {
+        animal: 'dog',
+        icon: `<i class="fas fa-dog"></i>`
+    },
+    {
+        animal:'dove',
+        icon: `<i class="fas fa-dove"></i>`
+    },
+    {
+        animal: 'fish',
+        icon: `<i class="fas fa-fish"></i>`
+    },
+    {
+        animal: 'frog',
+        icon: `<i class="fas fa-frog"></i>`
+    },
+    {
+        animal: 'hipo',
+        icon: `<i class="fas fa-hippo"></i>`
+    },
+    {
+        animal: 'horse',
+        icon: `<i class="fas fa-horse"></i>`
+    },
+    {
+        animal: 'spider',
+        icon:  `<i class="fas fa-spider"></i>`
+    },
+    {
+        animal: 'paw',
+        icon: `<i class="fas fa-paw"></i>`
+    },
+    {
+        animal: 'bird',
+        icon:  `<i class="fas fa-kiwi-bird"></i>`
+    },
+    {
+        animal:'dragon',
+        icon: `<i class="fas fa-dragon"></i>`
+    }
+]
+
