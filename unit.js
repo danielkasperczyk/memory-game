d5a46007dd10f09d58646ffe2b5e3d2f737f4dcb
danielkasperczyk <danielkasperczyk@gmail.com>
2020-12-01 18:31:22 +0100

    showing game board on picked level

--- unit.js
+++ unit.js
@@ -1,5 +1,5 @@
-import { levels } from './queries.js';
-
+import { game, gameBoard } from './queries.js';
+import { icons } from './icons.js';
 //jeżeli w LocalStorage będzie napisane w ile ruchów użytkwnik wygrał, należy to wyświetlić
 //a jeśli nie ma to nie należy wyświetlać
 
@@ -14,9 +14,31 @@ const test = {
 }
 localStorage.setItem('test', JSON.stringify(test) );
 
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
 
+export function getIcons(number){
+    const arrayOfIcons = icons.filter((item, index) => index < number/2)
+    .reduce(function (res,current){
+        return res.concat([current,current]);
+    },[]);
+    return shuffle(arrayOfIcons);
+}
+
+// Read and show records saved in localStorage
 //DONE
-
 export function showRecords(){
     const locStorage = Object.entries(JSON.parse(localStorage.getItem('test')));
     locStorage.filter(item => item[1] > 0)
@@ -25,4 +47,30 @@ export function showRecords(){
             levelBox.querySelector('p').classList.remove('hideRecord');
             levelBox.querySelector('.amount').textContent = `${record[1]}`
     })
+}
+
+//After clicking on one of few levels show section with cards and hide with levels to pick
+export function displaySection(hideSection, showSection){
+    hideSection.style.display = 'none';
+    showSection.style.display = 'flex';
+}
+
+export function createBoard(amountOfCards, icons) {
+    gameBoard.innerHTML = '';
+    for(let i = 0; i < amountOfCards; i++){
+        gameBoard.insertAdjacentElement('afterbegin', createCard(icons[i]))
+    }
+}
+
+function createCard( {animal, icon} ){
+    const button = document.createElement("button");
+    button.setAttribute("animal", `${animal}`)
+    button.classList.add('card');
+    const markup = `<div class="card__face card__face-front"></div>
+    <div class="card__face card__face-back">
+        ${icon}
+    </div>`
+    button.insertAdjacentHTML("afterbegin",markup)
+    console.log(button)
+    return button;
 }
