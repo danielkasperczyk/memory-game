b836c6af26d05a83f4866247c12bbf2c4b442115
danielkasperczyk <danielkasperczyk@gmail.com>
2020-12-02 21:15:58 +0100

    new queries

--- queries.js
+++ queries.js
@@ -2,3 +2,6 @@ export const levels = Array.from(document.querySelectorAll('.level'));
 export const menu = document.querySelector('.menu');
 export const game = document.querySelector('.game');
 export const gameBoard = document.querySelector('.game__cards');
+export const exitButton = document.querySelector('.panel__points-exit');
+export const points = document.querySelector('.points');
+export const moves = document.querySelector('.moves');
